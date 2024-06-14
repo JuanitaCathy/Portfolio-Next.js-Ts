006cefc59e5dae119da34e6583b9935e74058e10
Juanita <anitarosary3@gmail.com>
2024-06-14 17:22:32 +0530

    resolved errors

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
             Hello there!
-            <br /> I'm
+            <br /> I am
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               Juanita Cathy
